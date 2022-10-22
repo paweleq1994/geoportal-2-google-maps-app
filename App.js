8a426d840a3a99c076ecad3f26bf7ec345da6ff5
paweleq1994 <51318617+paweleq1994@users.noreply.github.com>
2022-10-22 15:27:30 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import proj4 from './assets/proj4'
 
 export default function App() {
-    const [text, setText] = useState("Chwaszczewo 15");
+    const [text, setText] = useState("");
     let [responseGeo, setResponseGeo] = useState([]);
 
     proj4.defs("EPSG:2180","+proj=tmerc +lat_0=0 +lon_0=19 +k=0.9993 +x_0=500000 +y_0=-5300000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs");
